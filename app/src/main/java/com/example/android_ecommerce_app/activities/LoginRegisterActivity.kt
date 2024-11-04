4ce941a594cd6fcd6b0b4f8af38acafa39869262
Drogbut <tchioidriss@yahoo.fr>
2024-11-04 16:27:31 +0100

    Add assets and folders structure

---
+++ app/src/main/java/com/example/android_ecommerce_app/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_ecommerce_app.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+
+class LoginRegisterActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
+}
